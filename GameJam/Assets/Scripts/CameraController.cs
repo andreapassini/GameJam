d5c237533d538154fc579c53a56e36f462cd7304
Andrea Passini <andreapa98@gmail.com>
2022-02-23 17:41:19 +0100

    SomeSprites

--- GameJam/Assets/Scripts/CameraController.cs
+++ GameJam/Assets/Scripts/CameraController.cs
@@ -7,6 +7,6 @@ public class CameraController : MonoBehaviour
      // Update is called once per frame
     void Update()
     {
-        transform.position = Player.position;
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
     }
 }
