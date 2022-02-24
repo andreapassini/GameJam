14b9ec029c23a60e74771958b910a98c3289e027
Andrea Passini <andreapa98@gmail.com>
2022-02-24 01:09:28 +0100

    Scritta

--- GameJam/Assets/Scripts/BedTrigger.cs
+++ GameJam/Assets/Scripts/BedTrigger.cs
@@ -2,14 +2,6 @@ using UnityEngine;
 
 public class BedTrigger : MonoBehaviour
 {
-
-	[SerializeField] LayerMask player;
-
-	private void Start()
-	{
-		//Physics2D.IgnoreLayerCollision(13, 7, true);
-	}
-
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		GameManager.PlayMainScene();
