92c1f5f90b564fe10148205da3d5fccca5169d8a
Andrea Passini <andreapa98@gmail.com>
2022-02-27 12:18:05 +0100

    UI

--- GameJam/Assets/Scripts/MovingTExt.cs
+++ GameJam/Assets/Scripts/MovingTExt.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class MovingTExt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _leftMove = false;
+
+    public Vector3 Space;
+
+    private void Start()
     {
-        
+        Space = new Vector3(1f, 0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+       
+        Move();
+    }
+
+    public void Move()
+    {
+        if(_leftMove)
+            transform.position += Space;
+
     }
 }
