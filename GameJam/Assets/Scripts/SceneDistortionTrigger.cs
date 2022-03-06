4c7f1fb3ce1ff084f91f4f33347e4967b6c91dd1
Andrea Passini <andreapa98@gmail.com>
2022-03-06 21:10:59 +0100

    Some Adjustments

--- GameJam/Assets/Scripts/SceneDistortionTrigger.cs
+++ GameJam/Assets/Scripts/SceneDistortionTrigger.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DistortionTrigger : MonoBehaviour
+public class SceneDistortionTrigger : MonoBehaviour
 {
-
     public GameManager GameManager;
 
+    // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameManager.PlayLvlDistorted();
+        GameManager.PlayLvlFinale();
     }
 }
